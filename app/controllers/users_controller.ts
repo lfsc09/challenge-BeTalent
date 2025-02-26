f7774201a4c30f4befd1e1d46ee805cfb9bb3ac1
Lucas Castro <lfsc09@gmail.com>
2025-02-26 12:05:28 -0300

    feat(controller): new `User` controller

---
+++ app/controllers/users_controller.ts
@@ -0,0 +1,30 @@
+import User from '#models/user'
+import { createUserValidator, updateUserValidator } from '#validators/user'
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class UsersController {
+  async allUsers({ response }: HttpContext) {
+    const users = await User.all()
+    return response.status(200).json(users)
+  }
+
+  async newUser({ request, response }: HttpContext) {
+    const input = await request.validateUsing(createUserValidator)
+    const output = await User.create(input)
+    return response.status(output.$isPersisted ? 201 : 400)
+  }
+
+  async editUser({ request, response, params }: HttpContext) {
+    const user = await User.findOrFail(params.id)
+    const input = await request.validateUsing(updateUserValidator)
+    user.merge(input)
+    await user.save()
+    return response.status(200)
+  }
+
+  async deleteUser({ response, params }: HttpContext) {
+    const user = await User.findOrFail(params.id)
+    await user.delete()
+    return response.status(200)
+  }
+}
