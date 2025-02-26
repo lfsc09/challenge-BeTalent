d4d635151f79d4c2a613689c60d99a820ca23157
Lucas Castro <lfsc09@gmail.com>
2025-02-26 16:44:11 -0300

    refactor(validator): added more constrains

--- app/validators/user.ts
+++ app/validators/user.ts
@@ -2,16 +2,16 @@ import vine from '@vinejs/vine'
 
 export const createUserValidator = vine.compile(
   vine.object({
-    email: vine.string().trim().email(),
-    password: vine.string().trim(),
+    email: vine.string().trim().minLength(1).maxLength(500).email(),
+    password: vine.string().trim().minLength(8),
     role: vine.string().trim().in(['ADMIN', 'MANAGER', 'FINANCE', 'USER']),
   })
 )
 
 export const updateUserValidator = vine.compile(
   vine.object({
-    email: vine.string().trim().email().optional(),
-    password: vine.string().trim().optional(),
+    email: vine.string().trim().minLength(1).maxLength(500).email().optional(),
+    password: vine.string().trim().minLength(8).optional(),
     role: vine.string().trim().in(['ADMIN', 'MANAGER', 'FINANCE', 'USER']).optional(),
   })
 )
