8f592d364b049d640a1ceedc1ec5e69b7eaf0c90
Lucas Castro <lfsc09@gmail.com>
2025-02-26 10:18:39 -0300

    chore: configured `japa` to use `expect` instead of `asset`

--- tests/bootstrap.ts
+++ tests/bootstrap.ts
@@ -1,4 +1,4 @@
-import { assert } from '@japa/assert'
+import { expect } from '@japa/expect'
 import { apiClient } from '@japa/api-client'
 import app from '@adonisjs/core/services/app'
 import type { Config } from '@japa/runner/types'
@@ -13,7 +13,7 @@ import testUtils from '@adonisjs/core/services/test_utils'
  * Configure Japa plugins in the plugins array.
  * Learn more - https://japa.dev/docs/runner-config#plugins-optional
  */
-export const plugins: Config['plugins'] = [assert(), apiClient(), pluginAdonisJS(app)]
+export const plugins: Config['plugins'] = [expect(), apiClient(), pluginAdonisJS(app)]
 
 /**
  * Configure lifecycle function to run before and after all the
