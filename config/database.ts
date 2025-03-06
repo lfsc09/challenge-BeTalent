e509ecd20b12aad8124a28fc68ff075fa49c5cfa
Lucas Castro <lfsc09@gmail.com>
2025-03-06 01:51:37 -0300

    chore: database connection now using connection pools

--- config/database.ts
+++ config/database.ts
@@ -17,6 +17,10 @@ const dbConfig = defineConfig({
         naturalSort: true,
         paths: ['database/migrations'],
       },
+      pool: {
+        min: 2,
+        max: 15,
+      },
     },
   },
 })
