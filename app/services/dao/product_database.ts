d38afdf53e84a7219539708747ad48e3a5c91c77
Lucas Castro <lfsc09@gmail.com>
2025-03-06 02:01:29 -0300

    refactor(product-dao): new method to retrieve products by its IDs

--- app/services/dao/product_database.ts
+++ app/services/dao/product_database.ts
@@ -4,7 +4,10 @@ import {
   UpdateProductDTO,
   ProductDAO,
   ProductDTO,
-} from '../../../../contracts/product_dao.js'
+  ProductToBePurchasedDTO,
+  ProductPurchasedDTO,
+} from '../../../contracts/product_dao.js'
+import { errors as lucidErrors } from '@adonisjs/lucid'
 
 export class ProductDAODatabase implements ProductDAO {
   async getAllProducts(): Promise<ProductDTO[]> {
@@ -14,6 +17,21 @@ export class ProductDAODatabase implements ProductDAO {
       amount: product.amount.toNumber(),
       createdAt: product.createdAt,
       updatedAt: product.updatedAt,
+    }))
+  }
+
+  async getProductsByIds(input: ProductToBePurchasedDTO[]): Promise<ProductPurchasedDTO[]> {
+    const products = await ProductModel.query().whereIn(
+      'id',
+      input.map((product) => product.productId)
+    )
+
+    if (products.length !== input.length) throw new lucidErrors.E_ROW_NOT_FOUND(ProductModel)
+
+    return products.map((product) => ({
+      id: product.id,
+      quantity: input.find((inputProduct) => inputProduct.productId === product.id)!.quantity,
+      amount: product.amount,
     }))
   }
 
